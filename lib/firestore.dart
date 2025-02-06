50e1cc3c520ea0f22d3af9cd96d4958476be1a9c
mura4906 <gkpj4906@yahoo.co.jp>
2025-02-06 23:45:05 +0900

    mainページ修正

--- lib/firestore.dart
+++ lib/firestore.dart
@@ -1,6 +1,5 @@
 import 'dart:core';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:my_web_app/models.dart';
 
 void fireBaseTestCall() async {
