396020a3720c3ba4852e325b263917d2c19aa2fc
mura4906 <gkpj4906@yahoo.co.jp>
2025-02-06 23:24:59 +0900

    firestoreクラス作成

---
+++ lib/firestore.dart
@@ -0,0 +1,66 @@
+import 'dart:core';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:my_web_app/models.dart';
+
+void fireBaseTestCall() async {
+  //await FirestoreService.instance.addKey('テストキー');
+  //await FirestoreService.instance.addResident('tester');
+}
+
+/// ファイアーストアと通信するクラス
+class FirestoreService {
+  FirestoreService._();
+  static FirestoreService instance = FirestoreService._();
+  final db = FirebaseFirestore.instance;
+
+  Future<void> addResident(String name) async {
+    await db
+        .collection('residents')
+        .add(Resident.register(name: name).toJson());
+  }
+
+  Future<void> addKey(String name) async {
+    await db
+        .collection('houseKeys')
+        .add(HouseKey.register(name: name).toJson());
+  }
+
+  Future<void> addResidentAll() async {
+    addResident('ゆう');
+    addResident('りさこ');
+    addResident('ひより');
+    addResident('あかね');
+    addResident('このか');
+    addResident('あみ');
+    addResident('のぞみ');
+    addResident('あおい');
+    addResident('なお');
+  }
+
+  Future<List<HouseKey>> readKeyList() async {
+    final doc = await db.collection('houseKeys').orderBy('name').get();
+    final keyList = doc.docs.map((e) {
+      return HouseKey.fromJson(e.id, e.data());
+    }).toList();
+    return keyList;
+  }
+
+  /* Read 読み出し */
+  Future<List<Resident>> readResidentList() async {
+    final doc = await db.collection('residents').orderBy('location').get();
+    final residentList = doc.docs.map((e) {
+      final i = Resident.fromJson(e.id, e.data());
+      return i;
+    }).toList();
+    return residentList;
+  }
+
+  Future<void> updateResident(Resident resident) async {
+    await db.collection('residents').doc(resident.id).update(resident.toJson());
+  }
+
+  Future<void> updateKey(HouseKey houseKey) async {
+    await db.collection('houseKeys').doc(houseKey.id).update(houseKey.toJson());
+  }
+}
